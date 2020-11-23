a3b5224a641942ea73cbbfe3bde53d911b86c891
1158814908 <1158814908@qq.com>
2020-11-23 08:34:06 +0800

    2020年11月23日

---
+++ 课堂文件/第14次课/all.js
@@ -0,0 +1,9 @@
+function orange(){
+    var array = [12,3,125,574,87,234]
+    for ( let index = 0; index < array.length;index++){
+        const element = array[index];
+        console.log(element);
+    }
+}
+
+orange();
